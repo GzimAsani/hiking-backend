4fa4338f2fceefc929f17ceee3d06c2270432dc7
selajdin89 <selajdinmemeti89@gmail.com>
2024-03-28 10:23:29 +0100

    Added new property for authorName on blog schema

--- src/models/Blogs.ts
+++ src/models/Blogs.ts
@@ -1,34 +1,37 @@
-import mongoose from "mongoose";
-import ImageSchema from "./image.model";
+import mongoose from 'mongoose';
+import ImageSchema from './image.model';
 
 const Blogs = new mongoose.Schema({
-    date: {
-        type: Date,
-        default: Date.now,
-        required: true
-    },
-    author: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    title: {
-        type: String,
-        required: true
-    },
-    content: {
-        type: String,
-    },
-    images: {
-        type: [ImageSchema]
-    },
-    seenCount: {
-        type: Number,
-        default: 0
-    }
-
+  date: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  authorName: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+  },
+  images: {
+    type: [ImageSchema],
+  },
+  seenCount: {
+    type: Number,
+    default: 0,
+  },
 });
 
-const BlogsModel = mongoose.model("Blogs", Blogs);
+const BlogsModel = mongoose.model('Blogs', Blogs);
 
 export default BlogsModel;
